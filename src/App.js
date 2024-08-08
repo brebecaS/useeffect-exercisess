dcef94ceae67a39c5a20685ca3329d5562350344
brebecaS <birleanu0rebeca@gmail.com>
2024-08-08 20:28:16 +0300

    event list

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Example0 from "./exemple/Exeplul0";
 import Example1 from "./exemple/Exemplu1";
 import Example2 from "./exemple/Exemplu2";
 import Example3 from "./exemple/Exemplu3";
+import Examplu4 from "./exemple/Exemplu4";
 
 function App() {
   return (
     <div className="App">
-      <Example3 />
+      <Examplu2 />
     </div>
   );
 }
--- src/exemple/Exemplu4.js
+++ src/exemple/Exemplu4.js
@@ -1,3 +1,33 @@
-const Examplu4 = () => {};
+import { useEffect, useState } from "react";
+
+const Examplu4 = () => {
+  const [count, setCount] = useState(0);
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", (e) => {
+      setCurrentWidth(window.innerWidth);
+    });
+
+    return () => {
+      window.removeEventListener("resize", (e) => {
+        setCurrentWidth(window.innerWidth);
+      });
+    };
+  }, []);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Buton apasat de <strong>{count}</strong> ori
+      </button>
+      <h1>{currentWidth}</h1>
+    </div>
+  );
+};
 
 export default Examplu4;
