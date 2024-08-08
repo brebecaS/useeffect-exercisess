81f9953605c9cbb73c82990485b2abccedd3eaf8
brebecaS <birleanu0rebeca@gmail.com>
2024-08-08 19:24:34 +0300

    work in progress

--- src/exemple/Exemplu3.js
+++ src/exemple/Exemplu3.js
@@ -1,18 +1,22 @@
 import "./style.css";
 import { useState, useEffect } from "react";
-
 // "https://fakestoreapi.com/products"
 // GET POST PUT DELETE
-
 function Example3() {
-  const products = [];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products").then(async (response) => {
+      const productsResponse = await response.json();
+      setProducts(productsResponse);
+      console.log(productsResponse);
+    });
+  }, []);
 
   return (
     <main>
       <h1>Exemplul 3</h1>
-
       <button onClick={() => {}}>Load Products</button>
-
       {products.map((product) => {
         return (
           <section key={product.id}>
@@ -24,5 +28,4 @@ function Example3() {
     </main>
   );
 }
-
 export default Example3;
