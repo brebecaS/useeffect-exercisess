09032fc75b7c380ff8a711678f5ea4635c9c3034
brebecaS <birleanu0rebeca@gmail.com>
2024-08-29 19:27:45 +0300

    posts load on page load

--- src/exemple/Exemplu2.js
+++ src/exemple/Exemplu2.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import useQuotes from "./hooks/useQuotes";
+import { useEffect, useState } from "react";
 
 // "https://jsonplaceholder.typicode.com/posts"
 // GET POST PUT DELETE
@@ -9,16 +10,31 @@ import useQuotes from "./hooks/useQuotes";
 // fetch quotes on button click
 // create quotes component
 function Example2() {
-  const posts = [{ title: "Title", body: "Body" }];
+  const [posts, setPosts] = useState([]);
+  const [loadPosts, setLoadPosts] = useState(false);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(
+      async (response) => {
+        setPosts(await response.json());
+      }
+    );
+  }, []);
 
   return (
     <main>
       <h1>Exemplul 2</h1>
 
-      {/* <button onClick={() => {}}>Load Quotes</button> */}
+      <button
+        onClick={() => {
+          setLoadPosts(!loadPosts);
+        }}
+      >
+        Load Posts
+      </button>
 
       {posts.map((post) => (
-        <section key={post.text}>
+        <section key={post.id}>
           <h3>
             <span>{post?.title}</span>
           </h3>
